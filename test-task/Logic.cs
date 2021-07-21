7ee7e92f2dfeb9ef67998c438b473f558166a8ab
Rodion Ivanov <dev.rabbit999@gmail.com>
2021-07-21 23:08:27 +0600

    first commit

---
+++ test-task/Logic.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_task
+{
+    class Logic
+    {
+        Dictionary<string, string> numbersTranslate = new Dictionary<string, string> //Создал словарь из цифр
+        {
+            {"ноль","0"},
+            {"один","1"},
+            {"одна","1"},
+            {"два","2"},
+            {"три","3"},
+            {"четыре","4"},
+            {"пять","5"},
+            {"шесть","6"},
+            {"семь","7"},
+            {"восемь","8"},
+            {"девять","9"},
+            {"десять","10"},
+            {"одиннадцать","11"},
+            {"двенадцать","12"},
+            {"тринадцать","13"},
+            {"четырнадцать","14"},
+            {"пятнадцать","15"},
+            {"шестнадцать","16"},
+            {"семьнадцать","17"},
+            {"восемнадцать","18"},
+            {"девятнадцать","19"},
+            {"двадцать","20"},
+            {"тридцать","30"},
+            {"сорок","40"},
+            {"пятьдесят","50"},
+            {"шестьдесят","60"},
+            {"семьдесят","70"},
+            {"восемьдесят","80"},
+            {"девяносто","90"},
+            {"сто","100"},
+            {"двести","200"},
+            {"триста","300"},
+            {"четыреста","400"},
+            {"пятьсот","500"},
+            {"шестьсот","600"},
+            {"семьсот","700"},
+            {"восемьсот","800"},
+            {"девятьсот","900"},
+            {"тысяча","1000"},
+            {"тысяч","1000"},
+            {"миллион","1000000"},
+            {"миллионов","1000000"}
+        };
+
+        public string FindNumbers(string input)
+        {
+            string tmp = "", tmpout = "", output = "";
+            foreach (string word in input.Split(' '))// Получаем строку и разбиваем ее на слова
+            {
+                if (numbersTranslate.TryGetValue(word, out tmp)) //Проходим по словарю и следуем по условию
+                {
+                    if (tmpout != "") tmpout += " ";
+                    tmpout += tmp;
+                }
+                else
+                {
+                    if (tmpout != "") output += " " + AddNumbers(tmpout);
+                    tmpout = "";
+                    if (output != "") output += " ";
+                    output += word;
+                }
+            }
+            if (tmpout != "")
+            {
+                tmpout = AddNumbers(tmpout);
+                if (output != "") output += " ";
+                output += tmpout;
+            }
+            return output;
+        }
+        private string AddNumbers(string input)
+        {
+            int output = 0;
+            int output2 = 0;
+            foreach (string num in input.Split(' '))
+            {
+                if (output > 999)
+                {
+                    output2 = output;
+                    output = 0;
+                }
+                if (Int32.Parse(num) > 999)
+                {
+                    output = output * Int32.Parse(num);
+                }
+                else
+                {
+                    output = output + Int32.Parse(num);
+                }
+            }
+            return (output + output2).ToString();
+        }
+    }
+}
---
+++ test-task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentenceArr = { "Он заплатил двадцать миллионов за три таких автомобиля", "Она купила квартиру за десять миллионов сто пятьдесят тысяч рублей", "Мама купила мне путевку за десять тысяч рублей в Севастополь, там сегодня сорок градусов тепла" };
+
+            Logic convertToNum = new Logic();
+
+            for (int i = 0; i < sentenceArr.Length; i++)
+            {
+                Console.WriteLine(sentenceArr[i]);
+                var res = convertToNum.FindNumbers(sentenceArr[i]);
+                Console.WriteLine(res);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
